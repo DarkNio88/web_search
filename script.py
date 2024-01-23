2266d6f6be08f97ed40dabefaaaf280cf3f6dc49
DarkNio <sonic-xx@hotmail.it>
2024-01-23 15:13:01 +0100

    Update script.py

--- script.py
+++ script.py
@@ -2,12 +2,12 @@ import gradio as gr
 import modules.shared as shared
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import NoSuchElementException,TimeoutException
 from googlesearch import search
 from fake_useragent import UserAgent
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox  import GeckoDriverManager
 import re
 import urllib
 import html2text
@@ -16,7 +16,7 @@ ua = UserAgent()
 user_agent = ua.random
 print(user_agent)
 search_access = True
-service = Service(ChromeDriverManager().install())
+service = Service(GeckoDriverManager().install())
 options = Options()
 options.add_argument('headless')
 options.add_argument(f'--user-agent={user_agent}')
@@ -35,7 +35,7 @@ options.add_argument('--remote-debugging-port=9222')
 
 def websearch_results(url):
 
-    driver = webdriver.Chrome(service=service,options=options)
+    driver = webdriver.firefox(service=service,options=options)
     driver.set_page_load_timeout(3)
     html = ""
     output = ""
@@ -80,12 +80,12 @@ def update_search_access(checkbox_value):
 def input_modifier(user_input, state):
     global search_access
     if search_access:
-        search_query = re.search(r'search\s+"([^"]+)"', user_input, re.IGNORECASE)
+        search_query = re.search(r'cerca\s+"([^"]+)"', user_input, re.IGNORECASE)
 
         if search_query:
             query = search_query.group(1)
-        elif user_input.lower().startswith("search"):
-            query = user_input.replace("search", "").strip()
+        elif user_input.lower().startswith("cerca"):
+            query = user_input.replace("cerca", "").strip()
         else:
             query = ""
 
@@ -93,7 +93,7 @@ def input_modifier(user_input, state):
             return user_input
         else:
             shared.processing_message = f"*Searching online for {query}*"
-            state["context_instruct"] += "The user question is in User question. Relevant search results are in the Google search results, this is up to date information. Be truthfull and follow what is provided in the Google search results. Use Google search results in the response."
+            state["context_instruct"] += "La domanda dell'utente è in Domanda dell'utente. I risultati di ricerca pertinenti si trovano nei risultati di ricerca di Google, si tratta di informazioni aggiornate. Sii sincero e segui ciò che viene fornito nei risultati di ricerca di Google. Utilizza i risultati di ricerca di Google nella risposta."
             try:
                 search_data = ""
                 for result in search(query, num_results=2):
@@ -103,13 +103,13 @@ def input_modifier(user_input, state):
                 print(type(e), e)
                 state[
                     "context"
-                ] += "Tell the user an error ocurred"
+                ] += "Informa l'utente che si è verificato un errore"
                 pass         
             if search_data=="":
                 print("No results found!")
                 state[
                     "context"
-                ] += "Tell the user no results were found"
+                ] += "Comunica all'utente che non sono stati trovati risultati"
                 user_prompt = f"User question: {user_input}\n Google search results: NO RESULTS FOUND"
             else:
                 search_data = search_data[:1024]
